4b1f7a573a0be754bd8c830d86fbfa0c8932c0f5
aaron <a.aa.akio.aaron@gmail.com>
2014-05-03 14:46:37 -0700

    Added game mode switching

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var TSEGame = require('./lib/game.js')
 
 game = new TSEGame()
+
+var element = document.getElementById('blocker')
+element.addEventListener( 'click', function(){
+  game.setMode('play')
+}, false)
+
 game.start()
