b995de3640702ba09fc958713c076455a71a6178
aaron <a.aa.akio.aaron@gmail.com>
2014-05-03 18:10:55 -0700

    Added TransformComponent for basic object manipulation

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 var TSEGame = require('./lib/game.js')
 
 game = new TSEGame()
-
-var element = document.getElementById('blocker')
-element.addEventListener( 'click', function(){
-  game.setMode('play')
-}, false)
-
 game.start()
